34a5991a7339d5f9e06c5b1fcc367eadab49a257
MatveyAF-PG <matvej4005@gmail.com>
2024-11-13 20:48:35 +0500

    Update 1.py

--- 1.py
+++ 1.py
@@ -3,16 +3,9 @@ salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 increase = 0.05  # Ежемесячный рост цен
 month = 0
-spend_month = spend
-while money_capital > spend_month:
-    for i in range(month):
-        if i > 0:
-            spend_month = spend_month + spend_month * increase
-            money_capital = (money_capital + salary) - spend
-    if money_capital > 0:
-        month += 1
-    else:
-        break
-
+while money_capital >= spend - salary:
+    money_capital += (salary - spend)
+    spend = spend + spend * increase
+    month += 1
 print("Количество месяцев, которое можно протянуть без долгов:", month)
 
